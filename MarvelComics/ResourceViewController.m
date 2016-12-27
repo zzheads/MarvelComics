e442449e441366547072c752b64b5163a4b7ae11
Alexey Papin <zzheads@gmail.com>
2016-12-27 21:59:57 +0300

    added progressView to ResourceViewController

--- MarvelComics/ResourceViewController.m
+++ MarvelComics/ResourceViewController.m
@@ -26,6 +26,10 @@
 - (id) initWithCharacter:(Character *)character :(ResourceType)resourceType {
     self = [super init];
     
+    self.progressView = [[UIProgressView alloc] init];
+    self.progressView.translatesAutoresizingMaskIntoConstraints = false;
+    self.progressView.progressTintColor = [UIColor whiteColor];
+    
     self.scrollView = [[UIScrollView alloc] init];
     self.scrollView.pagingEnabled = true;
     self.scrollView.translatesAutoresizingMaskIntoConstraints = false;
@@ -52,6 +56,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    float width = [UIScreen mainScreen].bounds.size.width;
     float height = [UIScreen mainScreen].bounds.size.height;
     
     self.navigationItem.title = [NSString stringWithFormat:@"%@ of %@", [AbstractStory nameOfResourceType:self.resourceType], self.character.name];
@@ -68,6 +73,14 @@
                                               [self.scrollView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor],
                                               [self.scrollView.bottomAnchor constraintEqualToAnchor:self.bottomLayoutGuide.topAnchor]
                                               ]];
+
+    [self.view addSubview:self.progressView];
+    [NSLayoutConstraint activateConstraints:@[
+                                              [self.progressView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:width/10],
+                                              [self.progressView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor constant:-width/10],
+                                              [self.progressView.centerYAnchor constraintEqualToAnchor:self.bottomLayoutGuide.topAnchor]
+                                              ]];
+
     
     [self.view addSubview:self.pageControl];
     [NSLayoutConstraint activateConstraints:@[
@@ -75,16 +88,22 @@
                                               [self.pageControl.rightAnchor constraintEqualToAnchor:self.view.rightAnchor],
                                               [self.pageControl.centerYAnchor constraintEqualToAnchor:self.topLayoutGuide.bottomAnchor constant:height*9/10],
                                               ]];
+    
+    
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     FinalPartialCompletionHandler finalHandler = ^void(NSArray *resArray) {
+        [self.progressView setHidden:true];
         self.countItems = resArray.count;
         self.pageControl.numberOfPages = resArray.count;
         [self setupScrollViewWithStories:resArray];
     };
+    ProgressDelegate setProgress = ^void(float progress) {
+        [self.progressView setProgress:progress animated:true];
+    };
     self.scrollView.delegate = self;
-    [self.apiClient fetchPages:CharacterComics offset:0 limit:20 total:100 resourceId:self.character.id parser:[Comic parser] progress:nil partialCompletion:nil finalCompletion:finalHandler];
+    [self.apiClient fetchPages:CharacterComics offset:0 limit:20 total:100 resourceId:self.character.id parser:[Comic parser] progress:setProgress partialCompletion:nil finalCompletion:finalHandler];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -93,13 +112,13 @@
 
 - (void)setupScrollViewWithStories:(NSArray *)stories {
     CGFloat width = self.scrollView.bounds.size.width;
-    CGFloat height = self.scrollView.bounds.size.height-100;
+    CGFloat height = self.scrollView.bounds.size.height-150;
     
     int totalWidth = 0;
     for (AbstractStory *story in stories) {
         PageView *pageView = [PageView loadFromNib];
         [pageView configureWithStory:story];
-        pageView.frame = CGRectMake(totalWidth, -100, width, height);
+        pageView.frame = CGRectMake(totalWidth, -150, width, height);
         [self.scrollView addSubview:pageView];
         totalWidth += pageView.bounds.size.width;
     }
